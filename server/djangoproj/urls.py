5e787336cdbf50161676e272b13184895ee23d46
wmalikkh <wmalikkh69@gmail.com>
2026-07-20 19:12:08 +0300

    Add About Us page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
